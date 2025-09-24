d18b980316eb73261c4544f421984cd3bfc27d9b
fb.lb <fb.lubre@free.fr>
2025-09-24 09:44:27 +0200

    Add email-check component and add a <p> in home component to display message from back end after email checking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Register } from './register/register';
+import { EmailCheck } from './email-check/email-check';
 
 export const routes: Routes = [
     {path: "", component: Home},
-    {path: "inscription", component: Register}
+    {path: "inscription", component: Register},
+    {path: "inscription/check-email/:token", component: EmailCheck},
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  success?: boolean;
+  message: string = "";
+  checkEmailClass: string = "";
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['success'] === undefined) {
+        this.checkEmailClass = 'no-display';
+      } else {
+        this.success = params['success'] === 'true';
+        this.message = params['message'];
+
+        if (this.success) {
+          this.checkEmailClass = 'success';
+        } else {
+          this.checkEmailClass = 'error';
+        }
+      }
+    });
+    
+    
+  }
 }
